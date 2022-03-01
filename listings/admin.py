592208560afd9c00535af82a50fd16a8f010fd19
kei_kusanagi <kei_kusanagi@outlook.comi>
2022-03-01 13:03:24 -0600

    intentando desplegar imagenes previas de las imagenes

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Listing
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor' , 'photo_main') 
+
+admin.site.register(Listing, ListingAdmin)
