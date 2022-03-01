3804153db0b215257e446ba2dc52d9df52b2a69e
kei_kusanagi <kei_kusanagi@outlook.comi>
2022-02-28 19:02:47 -0600

    porfin quedo la base de datos

--- btre/settings.py
+++ btre/settings.py
@@ -71,8 +71,12 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD': 'kei_kusanagi',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
