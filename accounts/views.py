1835608ba551f3f945bb86c76dfa37863209696d
kei_kusanagi <kei_kusanagi@outlook.comi>
2022-03-09 13:05:22 -0600

    se agrega el metodo de registro y funciona

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        print('SUBMITTED REG')
+        return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
 
 def login(request):
     return render(request, 'accounts/login.html')
@@ -11,3 +15,5 @@ def logout(request):
 
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')    
+
+    #cap 8 ep 49 // 7:08
