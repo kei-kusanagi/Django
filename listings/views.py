0d01cf43ceb66412dae6bb6559f22398f191744a
kei_kusanagi <kei_kusanagi@outlook.comi>
2022-03-08 21:43:17 -0600

    añadiendo bien la funcion de busqueda

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,4 @@
+from ast import keyword
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .choices import price_choices, bedroom_choices, state_choices
@@ -27,11 +28,21 @@ def listing(request, listing_id):
 
     return render(request, 'listings/listing.html', context)
 
+
 def search(request):
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    # Keywords
+    if 'keywords' in request.GET
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__)
+
     context = {
         'state_choices': state_choices,
         'bedroom_choices': bedroom_choices,
-        'price_choices': price_choices
+        'price_choices': price_choices,
+        'listings': queryset_list
     }
 
     return render(request, 'listings/search.html', context)
