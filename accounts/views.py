015d9cb01d8cc5f77d78fb9fe7886ed8742a8a5b
kei_kusanagi <kei_kusanagi@outlook.comi>
2022-03-10 19:06:55 -0600

    queda funcional el LOGOUT

--- accounts/views.py
+++ accounts/views.py
@@ -58,9 +58,12 @@ def login(request):
         return render(request, 'accounts/login.html')
 
 def logout(request):
-    return redirect('index')
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, 'You are now logged out')
+        return redirect('index')
+    
 
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')    
 
-# cap 8 3:31
